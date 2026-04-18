3474b20e9009c543c7bbb3d76d2532b3fe06e0e8
dibashgautam <dibashgautam143@gmail.com>
2026-04-18 22:24:31 +0545

    Add STATIC_ROOT for production static files

--- megaglobal/settings.py
+++ megaglobal/settings.py
@@ -141,14 +141,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_ROOT = BASE_DIR / 'media'
-MEDIA_URL = '/media/'
-
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 
 CKEDITOR_5_CONFIGS = {
